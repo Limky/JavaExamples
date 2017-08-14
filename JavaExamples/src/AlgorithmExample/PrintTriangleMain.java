cb408446e24bab0065f892b9adb3f81a4fc1f49a
Limky <hot8118@naver.com>
2017-08-15 01:15:44 +0900

    init added

---
+++ JavaExamples/src/AlgorithmExample/PrintTriangleMain.java
@@ -0,0 +1,24 @@
+package AlgorithmExample;
+
+public class PrintTriangleMain {
+
+	public static void main(String[] args) {
+		PrintTriangle pt = new PrintTriangle();
+		System.out.println( pt.printTriangle(3) );
+	}
+}
+
+ class PrintTriangle {
+	public String printTriangle(int num){
+		String str = "";
+	  	for(int i = 0 ; i < num ; i ++){
+	  		for(int j = 0; j < i + 1; j ++) {
+	  			str +="*";
+	  			
+	  		}
+	    
+    	str +="\n";
+    }
+		return str;		
+	}
+ }
