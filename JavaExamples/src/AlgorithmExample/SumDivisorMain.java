580022ca08e3ab31c9f4018ba64fc993a8377942
Limky <hot8118@naver.com>
2017-08-17 01:59:09 +0900

    약수들의 합 added

---
+++ JavaExamples/src/AlgorithmExample/SumDivisorMain.java
@@ -0,0 +1,20 @@
+package AlgorithmExample;
+
+public class SumDivisorMain {
+
+	public static void main(String[] args) {
+		SumDivisor c = new SumDivisor();
+		System.out.println(c.sumDivisor(12));
+	}
+}
+
+class SumDivisor {
+	public int sumDivisor(int num) {
+		int answer = 0;
+		for(int i = 1; i <= num; i++) {
+			if(num % i == 0) answer+=i;
+		}
+		
+		return answer;
+	}
+}
